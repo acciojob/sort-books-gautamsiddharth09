409ec9968e4fd8489fb2348114f7f6060cefc78b
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-01-17 18:02:23 +0530

    done

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -27,7 +27,7 @@ const BookList = () => {
 
    return (
     <div className="books-container">
-      <h1>📚 Book List</h1>
+      <h1>Book List</h1>
 
       <div className="controls">
         <label>
