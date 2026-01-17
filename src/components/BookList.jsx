74ad1094ad36a680d4c55b1c80b21a9cdfbd300e
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-01-17 17:44:57 +0530

    done

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,57 +1,57 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { sortBookSelector } from '../features/bookSelector'
-import { setSortBy, setOrder, booksApi } from '../features/bookSlice'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sortBookSelector } from "../features/bookSelector";
+import { setSortBy, setOrder, booksApi } from "../features/bookSlice";
 
 const BookList = () => {
+  console.log("RENDER");
 
-   console.log("RENDER"); 
-   
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const books = useSelector(sortBookSelector)
+  const books = useSelector(sortBookSelector);
   const { isLoading, error, sortBy, order } = useSelector(
-    (state) => state.books
-  )
-useEffect(() => {
-  console.log("DISPATCHING API");
-  dispatch(booksApi());
-}, [dispatch]);
+    (state) => state.books,
+  );
+  useEffect(() => {
+    console.log("DISPATCHING API");
+    dispatch(booksApi());
+  }, [dispatch]);
 
-  if (isLoading) return <p>Loading...</p>
-  if (error) return <p>Error: {error}</p>
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
-    
-    <div>
-      <h1>Books List</h1>
-     
+    <div className="books-container">
+      <h1>📚 Book List</h1>
 
-     <label htmlFor="sortBy" data-cy="sortByLabel">Sort by:</label>
-<select
-  id="sortBy"
-  data-cy="sortBySelect"
-  value={sortBy}
-  onChange={(e) => dispatch(setSortBy(e.target.value))}
->
-  <option value="title">Title</option>
-  <option value="author">Author</option>
-  <option value="publisher">Publisher</option>
-</select>
+      <div className="controls">
+        <label>
+          Sort by:
+          <select
+            data-test="sort-select"
+            value={sortBy}
+            onChange={handleSortChange}
+          >
+            <option value="title">Title</option>
+            <option value="author">Author</option>
+            <option value="publisher">Publisher</option>
+          </select>
+        </label>
 
-<label htmlFor="order" data-cy="orderLabel">Order:</label>
-<select
-  id="order"
-  data-cy="orderSelect"
-  value={order}
-  onChange={(e) => dispatch(setOrder(e.target.value))}
->
-  <option value="asc">Ascending</option>
-  <option value="desc">Descending</option>
-</select>
+        <label>
+          Order:
+          <select
+            data-test="order-select"
+            value={order}
+            onChange={handleOrderChange}
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
+      </div>
 
-
-      <table>
+      <table data-test="books-table">
         <thead>
           <tr>
             <th>Title</th>
@@ -60,20 +60,19 @@ useEffect(() => {
             <th>ISBN</th>
           </tr>
         </thead>
-
         <tbody>
-          {books?.map((book) => (
-            <tr key={book.primary_isbn13}>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              <td>{book.publisher}</td>
-              <td>{book.primary_isbn13}</td>
+          {sorted.map((b) => (
+            <tr key={b.isbn}>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.publisher}</td>
+              <td>{b.isbn}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default BookList
+export default BookList;
